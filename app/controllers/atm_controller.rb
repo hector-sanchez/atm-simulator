41b194a9e8500ae253d94f6e691bfa4b1faece61
Hector Sanchez <sanchez.hector.m@gmail.com>
2025-08-06 12:51:53 -0400

    Fixes the stale atm location session

--- app/controllers/atm_controller.rb
+++ app/controllers/atm_controller.rb
@@ -26,9 +26,9 @@ class AtmController < ApplicationController
   end
 
   def assign_atm_location
-    # Only assign ATM once per session
-    unless session[:atm_machine_id]
-      location_data = SessionLocationService.assign_atm_for_session
+    # Only assign ATM once per session, but handle stale session data
+    unless session[:atm_machine_id] && AtmMachine.exists?(session[:atm_machine_id])
+      location_data = AtmLocator.find_nearest_atm
 
       session[:atm_machine_id] = location_data[:atm_machine].id
       session[:user_location] = location_data[:user_location]
@@ -38,5 +38,16 @@ class AtmController < ApplicationController
     @current_atm = AtmMachine.find(session[:atm_machine_id])
     @user_location = session[:user_location]
     @selection_reason = session[:selection_reason]
+  rescue ActiveRecord::RecordNotFound
+    # Handle case where stored ATM ID is invalid (e.g., after database reset)
+    location_data = AtmLocator.find_nearest_atm
+
+    session[:atm_machine_id] = location_data[:atm_machine].id
+    session[:user_location] = location_data[:user_location]
+    session[:selection_reason] = location_data[:selection_reason]
+
+    @current_atm = location_data[:atm_machine]
+    @user_location = location_data[:user_location]
+    @selection_reason = location_data[:selection_reason]
   end
 end
